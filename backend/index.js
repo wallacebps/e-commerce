2bd2917285ed65686b86fb1fc925d2f7ff01d7f2
Wallace Bruno <wallace.bruno@geocode.com.br>
2024-11-29 11:01:20 -0300

    fixed running port

--- backend/index.js
+++ backend/index.js
@@ -114,4 +114,8 @@ function verifyToken(req, res, next) {
   }
 }
 
-app.listen(5000);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
