9a5f81ea9d7df4d8ad74b9967f62ce4de6848802
Wallace Bruno <wallacebps@hotmail.com>
2024-12-02 09:24:36 -0300

    added margin-top on signup and login components

--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -51,7 +51,7 @@ const SignUp = () => {
   };
 
   return (
-    <div className="container mx-auto p-6 bg-gray-100 rounded-md shadow-md max-w-md">
+    <div className="container mx-auto mt-12 p-6 bg-gray-100 rounded-md shadow-md max-w-md">
       <h1 className="text-2xl font-semibold text-gray-800 mb-6 text-center">
         Sign Up
       </h1>
