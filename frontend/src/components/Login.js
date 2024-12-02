79461f12dc29a2c247324fca52748c949f17b1f4
Wallace Bruno <wallace.bruno@geocode.com.br>
2024-12-02 09:24:36 -0300

    added margin-top on signup and login components

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -50,7 +50,7 @@ const Login = () => {
   };
 
   return (
-    <div className="container mx-auto p-6 bg-gray-100 rounded-md shadow-md max-w-md">
+    <div className="container mx-auto mt-12 p-6 bg-gray-100 rounded-md shadow-md max-w-md">
       <h1 className="text-2xl font-semibold text-gray-800 mb-6 text-center">
         Login
       </h1>
